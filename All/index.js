bf80dab71bca86096a3f3ce077ec81c12c5c1fb6
UjeborMarvellous <92124127+UjeborMarvellous@users.noreply.github.com>
2023-05-22 10:52:46 +0100

    first commit

---
+++ All/index.js
@@ -0,0 +1,6 @@
+let toggleIcon = document.getElementById("toggleIcon");
+let anchors = document.getElementById("Links");
+
+toggleIcon.addEventListener("click", function() {
+    anchors.classList.toggle("show")
+})
